2c6e3b970afadfc0b06735b568fdcc0bd1512268
pengpeng.wang.o <pengpeng.wang.o@nio.com>
2019-06-20 09:28:30 +0800

    添加日志aop

--- src/main/java/com/algorithm/AlgorithmApplication.java
+++ src/main/java/com/algorithm/AlgorithmApplication.java
@@ -3,7 +3,10 @@ package com.algorithm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
 @SpringBootApplication
+@EnableAspectJAutoProxy
 @MapperScan(basePackages = "com.algorithm.*.mapper")
 public class AlgorithmApplication {
 
