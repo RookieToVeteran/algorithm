909a68353ad1532e8af87b78204c3ef3ca68b3ff
pengpeng.wang.o <pengpeng.wang.o@nio.com>
2019-07-29 17:06:23 +0800

    直接插入排序

--- src/main/java/com/algorithm/test/TestAlgorithm.java
+++ src/main/java/com/algorithm/test/TestAlgorithm.java
@@ -4,6 +4,8 @@ import com.algorithm.utils.algorithm.CommonAlgorithms;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Random;
+
 /**
  * @program: algorithm
  * @description: 算法测试
@@ -17,6 +19,8 @@ public class TestAlgorithm {
     public static void main(String[] args) {
         //二分查找
         testBinarySearch();
+        //插入排序
+        testInsertSort();
     }
 
 
@@ -33,5 +37,18 @@ public class TestAlgorithm {
         LOGGER.info("******二分查找结果【{}】******",result);
     }
 
+
+    private static void testInsertSort(){
+        int[] targetArray = new int[30];
+        Random random = new Random();
+        for (int i = 0; i < 30; i++) {
+            int i1 = random.nextInt(50);
+            targetArray[i] = i1;
+        }
+        CommonAlgorithms.insertSort(targetArray);
+        for (int i : targetArray) {
+            System.out.println(i);
+        }
+    }
 
 }
--- src/main/java/com/algorithm/utils/algorithm/CommonAlgorithms.java
+++ src/main/java/com/algorithm/utils/algorithm/CommonAlgorithms.java
@@ -47,4 +47,43 @@ public class CommonAlgorithms {
     /**
      * 排序算法
      */
+
+    /**
+     *  直接插入排序 ---把n个待排序的元素看成为一个有序表和一个无序表。开始时有序表中只包含1个元素，
+     *  无序表中包含有n-1个元素，排序过程中每次从无序表中取出第一个元素，将它插入到有序表中的适当位置，
+     *  使之成为新的有序表，重复n-1次可完成排序过程。
+     *  它是一种稳定的排序算法，特别适合于待排序序列基本有序的情况
+     * @param targetArray
+     */
+    public static void insertSort(int[] targetArray){
+       int i , j , k;
+        for ( i = 1; i < targetArray.length; i++) {
+            //为targetArray[i]在前面的targetArray[0...i-1]有序区间中找一个合适的位置
+            for ( j = i - 1; j >= 0 ; j--) {
+                if(targetArray[j] < targetArray[i]){
+                    break;
+                }
+            }
+            //如找到了一个合适的位置
+            if (j != i - 1) {
+                //将比i大的数往后移
+                int temp = targetArray[i];
+                for ( k = i-1; k > j; k--) {
+                   targetArray[k+1] = targetArray[k];
+                    targetArray[k] = temp;
+                }
+            }
+        }
+    }
+
+
+    public static void bubbleSort(int[] targetArray){
+
+
+
+    }
+
+
+
+
 }
