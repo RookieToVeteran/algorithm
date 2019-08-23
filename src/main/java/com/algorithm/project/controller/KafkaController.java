4c4793ebc86502b7779a111f23e792ea7eade923
pengpeng.wang.o <pengpeng.wang.o@nio.com>
2019-08-23 10:13:18 +0800

    优化

--- src/main/java/com/algorithm/project/controller/KafkaController.java
+++ src/main/java/com/algorithm/project/controller/KafkaController.java
@@ -1,4 +1,4 @@
-package com.algorithm.controller;
+package com.algorithm.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
