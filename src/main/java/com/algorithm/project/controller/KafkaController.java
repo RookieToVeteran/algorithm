7bc5e25803d3a599e650141e87384580baf798cc
狂风飞我 <1422670632@qq.com>
2020-08-09 13:04:04 +0800

    add dependency

--- src/main/java/com/algorithm/project/controller/KafkaController.java
+++ src/main/java/com/algorithm/project/controller/KafkaController.java
@@ -23,7 +23,7 @@ public class KafkaController {
     @GetMapping("/message/send")
     public boolean send(@RequestParam String message){
         System.out.println("=============向生产者推送消息--------开始");
-        kafkaTemplate.send("testTopic",message);
+//        kafkaTemplate.send("testTopic",message);
         System.out.println("=============向生产者推送消息--------完成");
         return true;
     }
